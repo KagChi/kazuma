5d5d6b29e3bd3adc382b0c22d4cded346d1b8159
カグチ <59391215+KagChi@users.noreply.github.com>
2020-11-25 18:03:28 +0700

    Update music.py

--- modules/music.py
+++ modules/music.py
@@ -25,7 +25,7 @@ class Music(commands.Cog):
 
         # Initiate our nodes. For this example we will use one server.
         # Region should be a discord.py guild.region e.g sydney or us_central (Though this is not technically required)
-       node = await self.bot.wavelink.initiate_node(host=f'{os.environ.get("host")}',
+        node = await self.bot.wavelink.initiate_node(host=f'{os.environ.get("host")}',
                                               port=8080,
                                               rest_uri=f'http://{os.environ.get("host")}:8080',
                                               password='youshallnotpass',
